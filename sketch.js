633752053b1b6c5b93b153cee30aab5de00a4826
SiennasGithub Gray <siennapark6@gmail.com>
2024-04-17 21:55:21 +0000

    added code

--- sketch.js
+++ sketch.js
@@ -1,26 +1,20 @@
-let hearts = [];
-let colors = [];
-
 function setup() {
   createCanvas(400, 400);
-  colors = ["pink", "red", "white", "darkred"];
 }
 
 function draw() {
-  background("#e5e1dc");
-  for (let i=0; i<hearts.length; i++) {
-    hearts[i].display();
-    hearts[i].fall();
-  }
+  background("black");
+  translate(width/2, height/2);
   
-  for (let i=0; i<hearts.length; i++) {
-    if (hearts[i].y > height+20) {
-      hearts.splice(i, 1);
-    }
+  fill("pink");
+  stroke("#F04C96");
+  strokeWeight(4);
+  beginShape();
+  for(var a = 0; a < TWO_PI; a+=0.01){
+    var r = 10;
+    var x = r*16 * pow(sin(a),3);
+    var y = -r * (13*cos(a) - 5*cos(2*a)-2 * cos(3*a) - cos(4*a));
+    vertex(x,y);
   }
-  
-}
-
-function mouseDragged() {
-  hearts.push(new Heart(mouseX, mouseY));
+  endShape();
 }
